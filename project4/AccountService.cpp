25a228220b09485beaeef6705fe8f1c654354404
jiahe000 <77484740+jiahe000@users.noreply.github.com>
2021-06-04 12:26:57 +0800

    Update AccountService.cpp

--- project4/AccountService.cpp
+++ project4/AccountService.cpp
@@ -25,10 +25,125 @@ AccountService::AccountService() : HttpService("/users") {
   
 }
 
-void AccountService::get(HTTPRequest *request, HTTPResponse *response) {
+/**
+ * @description: Fetches the user object for this account. You can only fetch the user object for a the account that you authenticated.
+ * @param {HTTPRequest} *request
+ * @param {HTTPResponse} *response
+ * email string
+ * balance int
+ * @return {*}
+ */
+void AccountService::get(HTTPRequest *request, HTTPResponse *response)
+{
+    auto user = this->getAuthenticatedUser(request);
+    // cout << "get account info" << endl;
 
+    // judege authenticated
+    if (user)
+    {
+        // judege the permission of user
+        vector<string> split_user_string = StringUtils::split(request->getUrl(), '/');
+        if (split_user_string.size() < 2) {
+            // request url is error
+            throw ClientError::badRequest();
+        } else {
+            if (split_user_string[1] != user->user_id) {
+                // current user permisson denied
+                throw ClientError::forbidden();
+            }
+        }
+        string email = user->email;
+        int balance = user->balance;
+        // reponse
+        Document document;
+        Document::AllocatorType &a = document.GetAllocator();
+
+        Value resData;
+        resData.SetObject();
+        resData.AddMember("email", email, a);
+        resData.AddMember("balance", balance, a);
+
+        document.Swap(resData);
+        StringBuffer buffer;
+        PrettyWriter<StringBuffer> writer(buffer);
+        document.Accept(writer);
+        response->setContentType("application/json");
+        response->setBody(buffer.GetString() + string("\n"));
+        return;
+    }
+    else
+    {
+        throw ClientError::unauthorized();
+        // cout << "no auth" << endl;
+        // response->setStatus(401);
+        // return;
+    }
 }
 
-void AccountService::put(HTTPRequest *request, HTTPResponse *response) {
+/**
+ * @description: Updates the information for a user.
+ * @param {HTTPRequest} *request
+ *  email string
+ * @param {HTTPResponse} *response
+ *  email string
+ *  balance int
+ * @return {*}
+ */
+void AccountService::put(HTTPRequest *request, HTTPResponse *response)
+{
+    auto user = this->getAuthenticatedUser(request);
+    // judege authenticated
+    if (user)
+    {
+        // judege the permission of user
+        vector<string> split_user_string = StringUtils::split(request->getUrl(), '/');
+        if (split_user_string.size() < 2) {
+            // request url is error
+            throw ClientError::badRequest();
+        } else {
+            if (split_user_string[1] != user->user_id) {
+                // current user permisson denied
+                throw ClientError::forbidden();
+            }
+        }
+        // get new email
+        WwwFormEncodedDict auth_dict = request->formEncodedBody();
+        string newEmail = auth_dict.get("email");
+        
+        // check the argument
+        if (newEmail == "") {
+            // email can not be empty
+            throw ClientError::badRequest();
+        }
 
+        // set new email
+        user->email = newEmail;
+
+        // get balance
+        int balance = user->balance;
+
+        // reponse
+        Document document;
+        Document::AllocatorType &a = document.GetAllocator();
+
+        Value resData;
+        resData.SetObject();
+        resData.AddMember("email", newEmail, a);
+        resData.AddMember("balance", balance, a);
+
+        document.Swap(resData);
+        StringBuffer buffer;
+        PrettyWriter<StringBuffer> writer(buffer);
+        document.Accept(writer);
+        response->setContentType("application/json");
+        response->setBody(buffer.GetString() + string("\n"));
+    }
+    else
+    {
+        throw ClientError::unauthorized();
+        // cout << "no auth" << endl;
+        // response->setStatus(401);
+        // return;
+    }
 }
+
